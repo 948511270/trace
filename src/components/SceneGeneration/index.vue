957fe7b52c8764f4f1e03f9cc8d517bea7ee1d04
Fu Yujie <35658119+948511270@users.noreply.github.com>
2024-01-25 21:36:26 +0800

    将生成场景封装成组件

--- src/components/SceneGeneration/index.vue
+++ src/components/SceneGeneration/index.vue
@@ -1,13 +1,75 @@
 <script setup lang="ts">
+import {  onMounted, ref, Ref } from 'vue';
+import request from '@/utils/request'
+
+// 获取场景图片 
+let imageUrlRef: Ref<string | undefined> = ref(undefined);
+const getScene = () => {
+
+  request.get("http://192.168.98.1:3000/getScene")
+    .then((res) => {
+      console.log("res", res.data.buffer);
+
+      // 他妈的，一开始 res.data.buffer.data数据是number类型数组，number数组不能转成blob
+      const uint8Array = new Uint8Array(res.data.buffer.data);
+      const blob = new Blob([uint8Array], { type: 'image/png' });
+      imageUrlRef.value = URL.createObjectURL(blob)
+
+      console.log("获取场景图片完毕");
+
+    }).catch((err) => {
+      console.log(err);
+
+    })
+}
+
+// 调整场景图片大小
+const containerDomRef = ref<HTMLElement | null>(null);
+const containerWidthRef = ref<number>(0);
+const containerHeightRef = ref<number>(0);
+
+const adjustImageSize = () => {
+  console.log("组件进入adjustImageSize");
+  
+  const container = containerDomRef.value;
+  if (container) {
+    containerWidthRef.value = container.clientWidth;
+    containerHeightRef.value = container.clientHeight;
+  }
+
+  const img = new Image();
+  img.src = imageUrlRef.value as string;
+
+  img.onload = () => {
+    console.log("进入img.onload");
+    console.log("containerDomRef",containerDomRef);
+    
+    if (containerDomRef.value) {
+      containerDomRef.value.style.width = '100%';
+      containerDomRef.value.style.height = '100%';
+    }
+  };
+}
+
+onMounted(() => {
+  console.log("组件onMounted");
+  getScene()
+});
 
 </script>
 
 <template>
-<div>
-    我是场景生成组件
-</div>
+<div class="scene">
+    <img v-if="imageUrlRef" :src="imageUrlRef" alt="场景图片，请等待加载..." ref="containerDomRef" class="responsive-image"
+      @load="adjustImageSize">
+  </div>
 </template>
 
 <style scoped lang="scss">
-
+.scene{
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+}
 </style>
--- src/pages/GamePage/index.vue
+++ src/pages/GamePage/index.vue
@@ -1,73 +1,20 @@
 <script setup lang="ts">
-import { getCurrentInstance, onMounted, onUpdated, onBeforeMount, ref, Ref } from 'vue';
-import request from '@/utils/request'
+import { getCurrentInstance, onMounted, onUpdated, onBeforeMount } from 'vue';
+import SceneGeneration from '@/components/SceneGeneration/index.vue';
+
 
 // 获取$router对象
 const instance = getCurrentInstance();
 const router = instance?.proxy?.$router;
+const mode = router?.currentRoute.value.query.mode
+// console.log("模式：", router?.currentRoute.value.query.mode);
 
-
-console.log("模式：", router?.currentRoute.value.query.mode);
-
-
-let imageUrlRef: Ref<string | undefined> = ref(undefined);
-const getScene = () => {
-
-  request.get("http://192.168.98.1:3000/getScene")
-    .then((res) => {
-      console.log("res", res.data.buffer);
-
-      // 他妈的，一开始 res.data.buffer.data数据是number类型数组，number数组不能转成blob
-      const uint8Array = new Uint8Array(res.data.buffer.data);
-      const blob = new Blob([uint8Array], { type: 'image/png' });
-      imageUrlRef.value = URL.createObjectURL(blob)
-
-
-    }).catch((err) => {
-      console.log(err);
-
-    })
-}
-
-
-const containerDomRef = ref<HTMLElement | null>(null);
-const containerWidthRef = ref<number>(0);
-const containerHeightRef = ref<number>(0);
-
-const adjustImageSize = () => {
-  const container = containerDomRef.value;
-  if (container) {
-    containerWidthRef.value = container.clientWidth;
-    containerHeightRef.value = container.clientHeight;
-  }
-
-
-  const img = new Image();
-  img.src = imageUrlRef.value as string;
-
-  img.onload = () => {
-
-    if (containerDomRef.value) {
-      containerDomRef.value.style.width = '100%';
-      containerDomRef.value.style.height = '100%';
-    }
-
-
-
-
-
-  };
-}
 
 onMounted(() => {
-  console.log("onMounted");
-  getScene()
+  
 });
 onUpdated(() => {
-  // URL.revokeObjectURL(imageUrl);
-  console.log("updated");
-  console.log("imageUrlRef", imageUrlRef.value);
-  // URL.revokeObjectURL(imageUrlRef.value as string)
+  
 })
 onBeforeMount(() => {
 
@@ -75,10 +22,9 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <div class="container">
-    <img v-if="imageUrlRef" :src="imageUrlRef" alt="场景图片，请等待加载..." ref="containerDomRef" class="responsive-image"
-      @load="adjustImageSize">
-
+  <!-- 如果mode是1，2，3时，才需要显示场景 -->
+  <div class="scene-generation" v-if="mode === '1' || mode === '2' || mode === '3' ">
+      <scene-generation></scene-generation>
   </div>
 </template>
 
@@ -88,18 +34,11 @@ html {
   height: 100%;
   width: 100%;
   margin: 0;
-  overflow: hidden;
 }
 
-.container {
+// 和scene-generation组件有关的css
+.scene-generation {
   text-align: center;
   height: 33vh;
-
-  .responsive-image {
-    max-width: 100%;
-    overflow: hidden;
-    display: inline-block;
-  }
-
 }
 </style>
